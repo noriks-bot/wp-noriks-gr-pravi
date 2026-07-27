f3da25499bb5da293e2241b054de421a573434fa
Noriks (via Claude) <info@noriks.com>
2026-07-27 07:50:16 +0200

    Thankyou upsell: prave 3x/5x SIVE kompozitne slike (generirane iz produktne, isti stil kot crne) v temi (img/upsell), URL prek get_template_directory_uri

--- wp-content/themes/noriks/woocommerce/checkout/thankyou.php
+++ wp-content/themes/noriks/woocommerce/checkout/thankyou.php
@@ -57,8 +57,8 @@ if ( $upsell_is_majice ) {
     $upsell_qty_names  = array( 1 => '1x Γκρι Μποξεράκια', 3 => '3x Γκρι Μποξεράκια', 5 => '5x Γκρι Μποξεράκια' );
     $upsell_qty_images = array(
         1 => 'https://noriks.com/gr/wp-content/uploads/2026/02/sive-boksarice-produktna.jpg',
-        3 => 'https://noriks.com/gr/wp-content/uploads/2026/02/sive-boksarice-produktna.jpg', // TODO: 3x siva kompozitna, ko obstaja
-        5 => 'https://noriks.com/gr/wp-content/uploads/2026/02/sive-boksarice-produktna.jpg', // TODO: 5x siva kompozitna, ko obstaja
+        3 => get_template_directory_uri() . '/img/upsell/siva-3x.png',
+        5 => get_template_directory_uri() . '/img/upsell/siva-5x.png',
     );
     $upsell_title_text = 'Προσθέστε μποξεράκια τώρα – 50% έκπτωση';
 }
